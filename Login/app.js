4e5efc1fef04f687e0fc49233c1f0ffbe5c901b8
Ronni92 <133808201+Ronni92@users.noreply.github.com>
2025-07-04 23:57:36 -0500

    change messaje.js

--- Login/app.js
+++ Login/app.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => res.send("Login funcionando"));
 
 // IMPORTANTE: escucha en el puerto correcto
 app.listen(3000, "0.0.0.0", () => {
-  console.log("Login service corriendo en http://0.0.0.0:3000");
+  console.log("Servidor escuchando...");
 });
 const loginRoutes = require("./routes/loginRoutes");
 app.use("/", loginRoutes);
