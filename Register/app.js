984b2fc846351399da85eb6f38ecaba1dcc6d373
Ronni92 <133808201+Ronni92@users.noreply.github.com>
2025-07-05 09:23:07 -0500

    update port

--- Register/app.js
+++ Register/app.js
@@ -7,9 +7,8 @@ const app = express();
 app.use(express.json());
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Registro de usuario escuchando en puerto ${PORT}`));
-
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Servidor Register activo en el puerto 3000");
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
+  console.log(`Registro de usuario escuchando en puerto ${PORT}`);
 });
+
