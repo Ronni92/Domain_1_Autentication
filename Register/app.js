ffc941f66bebaefb073d7e257cb1c1a3f33291f9
Ronni92 <133808201+Ronni92@users.noreply.github.com>
2025-07-05 08:22:35 -0500

    Create workflows for register"

--- Register/app.js
+++ Register/app.js
@@ -9,3 +9,7 @@ app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Registro de usuario escuchando en puerto ${PORT}`));
+
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Servidor Register activo en el puerto 3000");
+});
