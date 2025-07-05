5b729e8067e75c60ec5d5ee5d9b6419de1988dba
Ronni92 <133808201+Ronni92@users.noreply.github.com>
2025-07-05 13:37:34 -0500

    correction grammar

--- Register/app.js
+++ Register/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const userRoutes = require('./routes/userroutes');
+const userRoutes = require('./routes/userRoutes');
 dotenv.config();
 const app = express();
 app.use(express.json());
