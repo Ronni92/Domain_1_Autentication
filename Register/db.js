3d37d9fc948ae6be551a1ba550ed5b593f77fb72
Ronni92 <133808201+Ronni92@users.noreply.github.com>
2025-07-05 09:14:35 -0500

    change

--- Register/db.js
+++ Register/db.js
@@ -1,12 +1,11 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
 });
+
+pool.connect()
+  .then(() => console.log(' Connected to PostgreSQL'))
+  .catch((err) => console.error(' PostgreSQL connection error:', err.message));
 
 module.exports = pool;
