02df54ee4c6532bc283f3b353f693074c7378d76
Ronni92 <133808201+Ronni92@users.noreply.github.com>
2025-07-04 22:39:04 -0500

    update file login

--- Login/deploy-login.sh
+++ Login/deploy-login.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Detener y eliminar el contenedor si existe
+# Detener y eliminar contenedor anterior si existe
 sudo docker stop login-service || true
 sudo docker rm login-service || true
 
-# Descargar la nueva imagen
-sudo docker pull <priscila454>/login-service
+# Descargar nueva imagen
+sudo docker pull priscila454/login-service
 
-# Ejecutar el contenedor
-sudo docker run -d --name login-service -p 3000:3000 <priscila454>/login-service
+# Ejecutar contenedor
+sudo docker run -d --name login-service -p 3000:3000 priscila454/login-service
