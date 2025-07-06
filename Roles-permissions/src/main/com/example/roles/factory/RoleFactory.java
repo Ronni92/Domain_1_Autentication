df0f52ee6a035a0d46e4d2a4f9c6f5e286ea1641
Ronni92 <133808201+Ronni92@users.noreply.github.com>
2025-07-06 08:39:04 -0500

    .jar file correction

--- Roles-permissions/src/main/com/example/roles/factory/RoleFactory.java
+++
@@ -1,9 +0,0 @@
-package com.example.roles.factory;
-
-import com.example.roles.model.Role;
-
-public class RoleFactory {
-    public static Role createRole(String roleName) {
-        return new Role(roleName);
-    }
-}
