c6c261b4323966db87640788bda3a2a888445074
Ronni92 <133808201+Ronni92@users.noreply.github.com>
2025-07-06 13:13:16 -0500

    update port domain1

--- Login/app.js
+++ Login/app.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const app = express();
 const swaggerDocs = require("./swagger");
-
 app.use(express.json());
 swaggerDocs(app); // activa /docs
-
 // Aquí puedes agregar tus rutas, ejemplo:
 app.get("/", (req, res) => res.send("Login funcionando"));
-
 // IMPORTANTE: escucha en el puerto correcto
 app.listen(3000, "0.0.0.0", () => {
   console.log("Servidor escuchando...");
--- Login/utils/jwt.js
+++ Login/utils/jwt.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-
 exports.generateToken = (payload) => {
   return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
 };
