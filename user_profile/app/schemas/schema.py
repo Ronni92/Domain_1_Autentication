349d88c9121a0836c1c287bc4540f1cf25efdd7f
Ronni92 <133808201+Ronni92@users.noreply.github.com>
2025-07-06 19:06:16 -0500

    Create user_profile.yml

--- user_profile/app/schemas/schema.py
+++ user_profile/app/schemas/schema.py
@@ -1,18 +1,7 @@
-from __future__ import annotations
 import strawberry
-from app.schemas.types import ProfileType, ProfileInput
-from app.services.profile_service import get_profile_by_email, update_profile_data
 
 @strawberry.type
 class Query:
-    @strawberry.field
-    async def get_profile(self, email: str) -> ProfileType | None:
-        return await get_profile_by_email(email)
+    hello: str = "Hola desde GraphQL"
 
-@strawberry.type
-class Mutation:
-    @strawberry.mutation
-    async def update_profile(self, input: ProfileInput) -> ProfileType:
-        return await update_profile_data(input)
-
-schema = strawberry.Schema(query=Query, mutation=Mutation)
+schema = strawberry.Schema(query=Query)
